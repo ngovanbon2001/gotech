2cf2760c18c3ea26a166009ada00cbeba532aafe
BONNV <ngovanbon2001@gmil.com>
2024-11-03 12:47:28 +0700

    ux-about

--- sources/components/PromoBanner.vue
+++ sources/components/PromoBanner.vue
@@ -2,7 +2,7 @@
   <div class="w-full flex justify-center items-center">
     <div :class="`w-5 md:w-[154px] h-[1px] ${white ? 'bg-black' : 'bg-[#ffffff]'}`"></div>
     <span
-      :class="`promo-banner text-center font-black text-2xl md:text-3xl md:px-4 px-1 uppercase ${white ? 'text-[#DC0F0F]' : 'text-[#ffffff]'}`"
+      :class="`font-[Roboto Slab] promo-banner text-center font-black text-2xl md:text-3xl md:px-4 px-1 uppercase ${white ? 'text-[#DC0F0F]' : 'text-[#ffffff]'}`"
     >
       {{ title }}
     </span>
@@ -25,7 +25,6 @@ defineProps({
 
 <style scoped>
 .promo-banner {
-  font-family: SFU Helvetica;
   font-size: 30px;
   font-weight: 900;
   line-height: 40.98px;
---
+++ sources/pages/about/index.vue
@@ -0,0 +1,230 @@
+<template>
+  <div v-loading="loading">
+    <section class="flex justify-center bg-[#F4F4F4]">
+      <Breadcrumb :items="breadcrumbItems" />
+    </section>
+    <section
+      class="bg-[url('/images/bg-about.png')] lg:h-[567px] flex justify-center lg:py-8"
+    >
+      <div
+        class="xl:w-[1000px] lg:w-[950px] 2xl:w-[1240px] w-full md:w-[700px] flex lg:flex-row flex-col-reverse gap-10"
+      >
+        <div class="lg:w-[55%] w-full lg:pl-12">
+          <img src="/images/about-1.png" alt="no-img" />
+        </div>
+        <div class="lg:pt-8 lg:w-[45%] w-full">
+          <div class="font-black lg:text-[28px] text-[22px] uppercase italic text-center font-[SFU Helvetica]">
+            CÙNG GOTECh <br />
+            AN TÂM TRÊN MỌI HÀNH TRÌNH
+          </div>
+          <div class="text-justify w-full py-4 text-sm">
+            Gotech đang nỗ lực kiến tạo sự an tâm tuyệt đối cho khách hàng qua
+            từng sản phẩm và dịch vụ. Với công nghệ tiên tiến, kiểm soát chất
+            lượng nghiêm ngặt và đội ngũ hỗ trợ chuyên nghiệp, chúng tôi đảm bảo
+            rằng mỗi sản phẩm không chỉ mang đến sự tiện nghi mà còn là niềm tin
+            và sự an toàn trên mọi hành trình của bạn. Khi chọn Gotech, bạn
+            không chỉ chọn sản phẩm, mà còn chọn sự bảo đảm vững chắc cho mỗi
+            chuyến đi.
+          </div>
+          <div class="w-full flex justify-center">
+            <el-button class="btn-red !h-10 md:w-[228px]">Hotline: 1900.633.642</el-button>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Lịch sử hình thành -->
+    <section class="flex justify-center py-4">
+      <div
+        class="xl:w-[1000px] lg:w-[950px] 2xl:w-[1240px] w-full md:w-[700px] grid lg:grid-cols-2 grid-cols-1 gap-4"
+      >
+      <div>
+        <div class="w-full flex justify-center items-center">
+          <div :class="`max-w-5 md:w-[154px] h-[1px] bg-black`"></div>
+          <span
+            :class="`promo-banner text-center font-black text-2xl md:text-3xl md:px-4 px-1 uppercase text-[#DC0F0F]`"
+          >
+            {{intro?.history?.[0]?.title}}
+          </span>
+          <div :class="`max-w-5 md:w-[154px] h-[1px] bg-black`"></div>
+        </div>
+        <div class="text-justify text-[14px] font-[Roboto Slab] py-4" v-html="intro?.history?.[0]?.content"></div>
+      </div>
+      <div>
+        <img :src="intro?.history?.[0]?.url" alt="">
+      </div>
+      </div>
+    </section>
+    <!-- End lịch sử hình thành -->
+    <!-- Đội ngũ sáng lập -->
+    <section class="py-4 bg-[#F4F4F4]">
+      <PromoBanner :title="'Đội ngũ sáng lập'" />
+      <div class="w-full flex justify-center py-4">
+        <div
+          class="xl:w-[1000px] lg:w-[950px] 2xl:w-[1240px] w-full md:w-[700px] grid lg:grid-cols-5 grid-cols-2 gap-4"
+        >
+          <div v-for="value in intro?.manager" :key="value">
+              <img class="max-w-[225px]" :src="value.img" alt="">
+              <div class="w-full text-center py-2 font-bold text-lg uppercase">{{ value.name }}</div>
+              <div class="w-full text-center text-sm">{{ value.position }}</div>
+              <div class="flex justify-center py-4">
+                <div class="w-[40%] border-t"></div>
+              </div>
+              <div class="w-full text-center text-sm">{{ value.exp }}</div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End đội ngũ sáng lập -->
+    <!-- Tầm nhìn sứ mệnh -->
+    <section class="py-4">
+      <PromoBanner :title="'Tầm nhìn - sứ mệnh - giá trị cốt lõi'" />
+      <div class="w-full flex justify-center py-4">
+        <div
+          class="xl:w-[1000px] lg:w-[950px] 2xl:w-[1240px] w-full md:w-[700px] grid lg:grid-cols-2 grid-cols-1 gap-4"
+        >
+          <div class="" v-for="value in listHistory" :key="value">
+            <div class="w-full flex justify-center items-center">
+              <div :class="`w-auto md:w-full h-[1px] bg-[#DC0F0F]`"></div>
+              <span
+                :class="`promo-banner whitespace-nowrap text-center font-black text-lg md:text-lg md:px-4 px-1 uppercase text-[#000000]`"
+              >
+                {{value?.title}}
+              </span>
+              <div :class="`w-auto md:w-full h-[1px] bg-[#DC0F0F]`"></div>
+            </div>
+            <div class="text-justify h-[200px] overflow-y-auto" v-html="value.content"></div>
+            <div>
+              <img class="w-full" :src="value.url" alt="no-img">
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="w-full flex justify-center py-4">
+        <div
+          class="xl:w-[1000px] lg:w-[950px] 2xl:w-[1240px] w-full md:w-[700px]"
+        >
+            <div class="w-full flex justify-center items-center">
+              <div :class="`w-auto md:w-full h-[1px] bg-[#DC0F0F]`"></div>
+              <span
+                :class="`promo-banner whitespace-nowrap text-center font-black text-lg md:text-lg md:px-4 px-1 uppercase text-[#000000]`"
+              >
+                Giá trị cốt lõi
+              </span>
+              <div :class="`w-auto md:w-full h-[1px] bg-[#DC0F0F]`"></div>
+            </div>
+            <div class="grid lg:grid-cols-5 grid-cols-1 gap-4 py-4">
+              <div class="flex w-full flex-col items-center" v-for="value in intro?.core_value" :key="value">
+                  <div class="w-[80px] h-[80px] border border-[#DC0F0F] rounded-[50%] flex justify-center items-center">
+                    <img :src="value.image" alt="no-img">
+                  </div>
+                  <div class="w-full text-center py-1 text-sm uppercase">{{ value.title }}</div>
+                  <div class="w-full flex justify-center">
+                    <div class="border w-[30%]"></div>
+                  </div>
+                  <div class="text-justify text-sm">{{ value.content }}</div>
+              </div>
+            </div>
+        </div>
+      </div>
+    </section>
+    <!-- End tầm nhìn sứ mệnh -->
+    <!-- Nhận xét chuyên gia -->
+    <section class="bg-[#F4F4F4] py-4">
+      <PromoBanner :title="'Nhận xét chuyên gia'" />
+      <div class="w-full flex justify-center">
+        <div
+          class="xl:w-[1000px] lg:w-[950px] 2xl:w-[1240px] w-full md:w-[700px] py-4"
+        >
+          <Carousel
+            :autoplay="2000"
+            v-bind="settings"
+            :breakpoints="breakpoints"
+          >
+            <Slide v-for="(value, index) in comment" :key="index">
+              <div
+                :title="value.product_name"
+                class="cursor-pointer carousel__item w-[217px] transition-transform duration-300 transform hover:scale-105"
+              >
+                <img
+                  :src="value.url"
+                  class="max-h-[232px] max-w-[295px] mb-3"
+                  alt="no-img"
+                />
+                <div
+                  class="font-bold text-base truncate w-full text-left uppercase"
+                >
+                  {{ value.description }}
+                </div>
+              </div>
+            </Slide>
+
+            <template #addons>
+              <Navigation />
+            </template>
+          </Carousel>
+        </div>
+      </div>
+    </section>
+    <!-- End nhận xét chuyên gia -->
+  </div>
+</template>
+<script setup>
+import { ref, computed, onMounted } from "vue";
+import "vue3-carousel/dist/carousel.css";
+import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel";
+import apiService from "@/service/service.ts";
+
+const breadcrumbItems = [
+  { text: "Home", to: "/" },
+  { text: "Về Gotech", to: "/san-pham" },
+];
+const intro = ref([]);
+const comment = ref([]);
+const listOrganization = ref([]);
+const breakpoints = {
+  300: {
+    itemsToShow: 1,
+    snapAlign: "center",
+  },
+  700: {
+    itemsToShow: 3,
+    snapAlign: "center",
+  },
+  1000: {
+    itemsToShow: 4,
+    snapAlign: "start",
+  },
+  1240: {
+    itemsToShow: 4,
+    snapAlign: "start",
+  },
+  1536: {
+    itemsToShow: 5,
+    snapAlign: "start",
+  },
+};
+
+const getIntro = async () => {
+  const response = await apiService.getAll("/intro");
+  intro.value = response.data.data;
+};
+const getOrganization = async () => {
+  const response = await apiService.getAll("/organization");
+  listOrganization.value = response.data.data;
+};
+const getComment = async () => {
+  const response = await apiService.getAll("/slide", { slide_type: 3 });
+  comment.value = response.data.data;
+};
+
+const listHistory = computed(() => {
+  return intro.value.history ? intro.value.history.slice(1, 3) : [];
+});
+
+onMounted(() => {
+  getIntro();
+  getOrganization();
+  getComment();
+});
+</script>
